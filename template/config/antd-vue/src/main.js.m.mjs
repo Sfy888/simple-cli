8ee0f53174767f12e5d1a2edbafa9dc6a85739b8
sunfuyu <sunfuyu@bwcj.com>
2024-04-07 15:04:18 +0800

    fix: 添加vue-js-template-antd-vue模板

--- template/config/antd-vue/src/main.js.m.mjs
+++ template/config/antd-vue/src/main.js.m.mjs
@@ -1,10 +1,9 @@
 export default function getData({ oldData }) {
-  const elementPlusPlugin = {
+  const antdVuePlugin = {
     name: 'antdVue',
     importers: [
       "import antdVue from 'ant-design-vue'",
       "import 'ant-design-vue/dist/reset.css'",
-      "import * as antDIconsVue from '@ant-design/icons-vue'"
     ],
     initializer: 'antdVue'
   }
--- template/config/antd/src/main.js.m.mjs
+++ template/config/antd/src/main.js.m.mjs
@@ -1,17 +1,15 @@
 export default function getData({ oldData }) {
-  const elementPlusPlugin = {
-    name: 'antdVue',
+  const antdPlugin = {
+    name: 'antd',
     importers: [
-      "import antdVue from 'ant-design-vue'",
-      "import 'ant-design-vue/dist/reset.css'",
-      "import * as antDIconsVue from '@ant-design/icons-vue'"
+      "import 'antd/dist/antd.css';",
+      "import 'ant-design/dist/reset.css'",
     ],
-    initializer: 'antdVue'
   }
 
   return {
     ...oldData,
     // Append the antdVue plugin right after the vue plugin
-    plugins: [...oldData.plugins, antdVuePlugin]
+    plugins: [...oldData.plugins, antdPlugin]
   }
 }
